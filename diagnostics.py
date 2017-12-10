863057dfd33e442d62e1ae166d36d955c5d04d41
anthony <anthony.chinwo@gmail.com>
2017-12-10 22:07:20 +0000

    - vary "thenWait" delay based on whether running in Pi

--- diagnostics.py
+++ diagnostics.py
@@ -1,14 +1,22 @@
 from motor import Motor
-from lib.gpio import Signal
+from lib.gpio import Signal, inPi
 
 
 def diagnostics():
     print('diagnostics starting...')
 
+    thenWait = 2 if inPi else 1
+
     with Signal.default().prepared():
         fwdBackMotor = Motor(Motor.MOVE)
+        turnMotor = Motor(Motor.TURN)
 
-        fwdBackMotor.moveForward(thenWait=2)
+        fwdBackMotor.moveForward(thenWait=thenWait)
+        fwdBackMotor.moveBack(thenWait=thenWait)
+
+        turnMotor.turnRight(thenWait=thenWait)
+        turnMotor.turnLeft(thenWait=thenWait)
+
         fwdBackMotor.stop()
 
 if __name__ == '__main__':
